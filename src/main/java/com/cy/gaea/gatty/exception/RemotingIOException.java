4a6dc97485adf38c711b69f5a3d51fcc73906781
chicc999 <123695646@qq.com>
2016-08-10 23:58:24 +0800

    完善远程IO异常

--- src/main/java/com/cy/gaea/gatty/exception/RemotingIOException.java
+++ src/main/java/com/cy/gaea/gatty/exception/RemotingIOException.java
@@ -6,4 +6,18 @@ import java.io.IOException;
  * Created by cy on 2016/7/10.
  */
 public class RemotingIOException extends IOException {
+	public RemotingIOException() {
+	}
+
+	public RemotingIOException(String message) {
+		super(message);
+	}
+
+	public RemotingIOException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public RemotingIOException(Throwable cause) {
+		super(cause);
+	}
 }
