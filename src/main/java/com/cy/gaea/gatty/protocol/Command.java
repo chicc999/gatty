9d52b9c6ffbcfb4909c9b94940becc572cd4a031
chicc999 <123695646@qq.com>
2016-08-16 00:23:11 +0800

    定义传输协议基类

--- src/main/java/com/cy/gaea/gatty/protocol/Command.java
+++ src/main/java/com/cy/gaea/gatty/protocol/Command.java
@@ -1,7 +1,21 @@
 package com.cy.gaea.gatty.protocol;
 
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
 /**
  * Created by cy on 2016/7/10.
  */
-public class Command {
+public abstract class Command {
+	private String requestId;
+
+	public Command() {
+		this.requestId = UUID.randomUUID().toString();
+	}
+
+	public String getRequestId() {
+		return requestId;
+	}
+
+	abstract ByteBuffer encode();
 }
